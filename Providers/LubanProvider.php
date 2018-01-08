f4b09f2d8fad8db27117359e0858b2ea4b27343b
Wanglei <flaboy.cn@gmail.com>
2018-01-08 15:32:31 +0800

    init

---
+++ Providers/LubanProvider.php
@@ -0,0 +1,11 @@
+<?php
+namespace Shopex\Luban\Framework\Providers;
+use Illuminate\Support\ServiceProvider;
+
+use Shopex\Luban\Client;
+class LubanProvider extends ServiceProvider{
+
+	public function register(){});
+	}
+
+}
